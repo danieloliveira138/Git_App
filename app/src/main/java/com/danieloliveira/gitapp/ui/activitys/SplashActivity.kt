b1fac6e858157b96e12a6f47d08b51d358e7b345
danieldejesusoliveira <daniel.oliveira@lab360.com.br>
2019-12-15 15:16:31 -0300

    SplashActivity.

--- app/src/main/java/com/danieloliveira/gitapp/ui/activitys/SplashActivity.kt
+++ app/src/main/java/com/danieloliveira/gitapp/ui/activitys/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.danieloliveira.gitapp.ui.activitys
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.danieloliveira.gitapp.R
@@ -9,5 +10,13 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        sendToMainActivity()
+    }
+
+    private fun sendToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        this.finish()
     }
 }
